ef0008d918d4eb903dff97b5f642aa55263d850b
김경훈 <kkh3363@gmail.com>
2026-03-29 23:30:04 +0900

    Create code0406.c

---
+++ source/ch04/code0406.c
@@ -0,0 +1,13 @@
+// Automatic conversion: int to float
+float myFloat = 9;
+
+printf("%f", myFloat); // 9.000000
+
+// Automatic conversion: float to int
+int myInt = 9.99;
+
+printf("%d", myInt); // 9
+
+float sum = 5 / 2;
+
+printf("%f", sum); // 2.000000
