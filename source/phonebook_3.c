0b81db2b1e16e13d11dc79e3fa2087a14b4e23ce
김경훈 <kkh3363@gmail.com>
2026-06-01 15:29:21 +0900

    Update phonebook_3.c

--- source/phonebook_3.c
+++ source/phonebook_3.c
@@ -1,3 +1,4 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <string.h>
 #define MAX 100
@@ -108,6 +109,7 @@ void readFile(){
 		}
 		
 	}
+	fclose(fpr);
 }
 int main(){
 	int menu;
